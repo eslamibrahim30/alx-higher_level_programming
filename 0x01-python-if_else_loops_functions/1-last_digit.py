4aa8afb9bb4eaffe63e770ea7537719b0bf13a8c
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-08-08 16:05:29 -0700

    Compare last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+print(f"Last digit of {number:d} ", end="")
 if number < 0:
     number *= -1
 last_digit = number % 10
-print(f"Last digit of {number:d} is {last_digit:d} ", end="")
+print(f"is {last_digit:d} ", end="")
 if last_digit > 5:
     print(f"and is greater than 5")
 elif last_digit == 0:
