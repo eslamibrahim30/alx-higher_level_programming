cfde158e2ec222cb5e2f2acb171f31cc8acbefd9
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-08-08 01:13:52 -0700

    Insert node in a sorted array

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * insert_node - Inserts a number into a sorted singly linked list.
+ * @head: a pointer to the head of the list
+ * @number: the given number to be added within the new added node.
+ *
+ * Return: the address of the new node, or NULL if it failed
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *node;
+	listint_t *ptr_cur;
+	listint_t *ptr_next;
+
+	if (head == NULL)
+		return (NULL);
+	node = (listint_t *)malloc(sizeof(listint_t));
+	if (node == NULL)
+		return (NULL);
+	node->n = number;
+	ptr_cur = *head;
+	ptr_next = ptr_cur->next;
+	if (node->n < ptr_next->n)
+	{
+		ptr_cur->next = node;
+		node->next = ptr_next;
+		return (node);
+	}
+	while (ptr_next != NULL)
+	{
+		if (ptr_cur->n <= node->n && ptr_next->n >= node->n)
+		{
+			ptr_cur->next = node;
+			node->next = ptr_next;
+			return (node);
+		}
+		ptr_cur = ptr_cur->next;
+		ptr_next = ptr_next->next;
+	}
+	ptr_cur->next = node;
+	node->next = NULL;
+	return (node);
+}
