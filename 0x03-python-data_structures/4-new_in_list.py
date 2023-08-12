0619356b4122533351a2deeb1c70a2af4944e00a
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-08-12 03:50:09 -0700

    Copy list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return copy_list
+    copy_list = []
+    for i in my_list:
+        copy_list.append(i)
+    copy_list[idx] = element
+    return copy_list
