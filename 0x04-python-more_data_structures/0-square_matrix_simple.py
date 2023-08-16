f629d786b3405fbda10205d5f6f8394d1a8090b0
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-08-16 00:46:36 -0700

    Square matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    if matrix is None:
+        return None
     res = [list(map(lambda x: x**2, matrix[i])) for i in range(len(matrix))]
     return res
