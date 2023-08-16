b0f75818efe3cff8d7c9c9be6f7448e8ea576fb2
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-08-16 07:40:53 -0700

    Print sorted dictionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,7 +2,8 @@
 def print_sorted_dictionary(a_dictionary):
     if a_dictionary is None or len(a_dictionary) == 0:
         return None
-    sorted_keys = sorted(list(a_dictionary))
+    sorted_keys = list(a_dictionary)
+    sorted_keys.sort()
     for key in sorted_keys:
         print('{} : {}'.format(key, a_dictionary[key]))
     return None
