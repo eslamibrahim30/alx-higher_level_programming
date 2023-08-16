47f02fec857d12bf2a26dcd91cbc4416c4de144f
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-08-16 06:34:56 -0700

    Get best score from a dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None or len(a_dictionary) == 0:
+    if a_dictionary is None:
         return None
     best_key = list(a_dictionary)[0]
     cur_best = a_dictionary[best_key]
