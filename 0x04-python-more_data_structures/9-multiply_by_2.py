0a2c64d3139785a045499128a1f12466f1f83538
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-08-16 09:02:24 -0700

    Multiply by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
+    if not isinstance(a_dictionary, dict):
+        return None
     new_dict = {}
     for key in a_dictionary:
         new_dict[key] = a_dictionary[key]*2
