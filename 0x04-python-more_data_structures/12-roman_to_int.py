475a9082f388eece19c63a7aa2cd9ba05b8cdef7
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-08-18 16:22:39 -0700

    Roman to integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    dict_romans = dict([('I', 1), ('V', 5), ('X', 10),
-        ('L', 50), ('C', 100), ('D', 500), ('M', 1000)])
+    romans_small = [('I', 1), ('V', 5)]
+    romans_mid = [('X', 10), ('L', 50)]
+    romans_big = [('C', 100), ('D', 500), ('M', 1000)]
+    dict_romans = dict(romans_small + romans_mid + romans_big)
     num = dict_romans[roman_string[-1]]
     for i in range(len(roman_string) - 1):
         if dict_romans[roman_string[i]] < dict_romans[roman_string[i + 1]]:
