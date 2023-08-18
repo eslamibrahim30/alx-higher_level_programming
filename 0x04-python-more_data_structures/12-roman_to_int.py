8dd744b60b847833f80caf4f9a8e969a82dd22cc
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-08-18 16:24:24 -0700

    Roman to integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if roman_string is None or  not isinstance(roman_string, str):
+        return None
     romans_small = [('I', 1), ('V', 5)]
     romans_mid = [('X', 10), ('L', 50)]
     romans_big = [('C', 100), ('D', 500), ('M', 1000)]
