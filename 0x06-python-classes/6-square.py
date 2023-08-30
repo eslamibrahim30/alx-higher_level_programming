7667f69737465d7600a9d0112ae31cd54ff72ec3
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-08-30 08:41:13 -0700

    Coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -58,14 +58,17 @@ class Square():
         value: the given value
     """
     def my_print(self):
-        for i in range(self.__position[1]):
+        if self.__size == 0:
             print()
-        for i in range(self.__size):
-            for j in range(self.__position[0]):
-                print(" ", end="")
-            for j in range(self.__size):
-                print("#", end="")
-            print()
+        else:
+            for i in range(self.__position[1]):
+                print()
+            for i in range(self.__size):
+                for j in range(self.__position[0]):
+                    print(" ", end="")
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
     """
     This class method prints in stdout the square with the character #
     """
