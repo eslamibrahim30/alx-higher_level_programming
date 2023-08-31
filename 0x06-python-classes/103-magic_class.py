25433278933bc4612fd7a2c181f582cc7804dc34
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-08-30 17:44:22 -0700

    Write a class with the same bytecode

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This module contains a magic class for a circle
+"""
+
+
+class MagicClass():
+    """
+    This is a magic class for a circle
+    It is used to create an instance of a circle in our program
+    """
+    def __init__(self, raduis=0):
+        if type(raduis) is not int:
+            raise TypeError('radius must be a number')
+        else:
+            self.__raduis = raduis
+    """
+    This class method initializes an inistance of the class.
+    Args:
+        raduis: the raduis of the circle.
+    """
+    def area(self):
+        return (self.__raduis ** 2) * math.pi
+    """
+    This class method calculates the area of the circle.
+    """
+    def circumference(self):
+        return 2 * math.pi * self.__raduis
+    """
+    This class method calculates the circumference of the circle.
+    """
