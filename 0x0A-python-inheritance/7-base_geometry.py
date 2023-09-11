04a2bd5b9a49272e535eb03c17563b3e6c3f4120
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-09-11 07:56:11 -0700

    Integer validator

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -15,7 +15,7 @@ class BaseGeometry:
     This method should calculates the area of a given object.
     """
     def integer_validator(self, name, value):
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
