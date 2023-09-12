2cb0cbf434d0a9926c970cda760ebc1f7cfc5d61
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-09-12 14:27:00 -0700

    Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module for reading a file.
+"""
+
+
+def read_file(filename=""):
+    """
+    This function reads a text file (UTF8) and prints it to stdout.
+    """
+    with open(filename, 'r', encoding="utf-8") as f:
+        lines = f.read()
+        print(lines, end="")
