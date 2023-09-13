17abe00bf1c4b006d7940ee73ac8d968cfa97ea4
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-09-13 05:17:09 -0700

    Class to JSON

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module for JSON serialization of an object.
+"""
+
+
+def class_to_json(obj):
+    """
+    This function returns the dictionary description
+    with simple data structure (list, dictionary, string,
+    integer and boolean) for JSON serialization of an object.
+    """
+    return vars(obj)
