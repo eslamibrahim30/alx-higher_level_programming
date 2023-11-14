71226fb6df461033ef12d54f4237c7f5c82e2fe4
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-11-14 14:29:22 -0800

    Square #1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,15 +2,12 @@
 const Square0 = require('./5-square');
 class Square extends Square0 {
   constructor (size) {
-    if (size > 0) {
-      super(size);
-      this.size = size;
-    }
+    super(size);
   }
 
   charPrint (c) {
-    for (let i = 0; i < this.size; i++) {
-      for (let j = 0; j < this.size; j++) {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
         if (c === undefined) {
           process.stdout.write('X');
         } else {
