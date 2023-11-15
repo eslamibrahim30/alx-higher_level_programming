17876769cf00cf1c508327d92f05f82fceb97b43
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-11-15 13:17:13 -0800

    Add

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+if (Number.isInteger(parseInt(process.argv[2])) && Number.isInteger(parseInt(process.argv[3]))) {
+  console.log(parseInt(process.argv[2]) + parseInt(process.argv[3]));
+} else {
+  console.log('NaN');
+}
