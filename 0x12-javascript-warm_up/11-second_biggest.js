f6efd2423a22b8ddbebe886fcfa84372a0bf0686
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-11-15 21:25:01 -0800

    Second biggest!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -7,7 +7,12 @@ if (process.argv.length === 2 || process.argv.length === 3) {
   for (let i = 2; i < process.argv.length; i++) {
     if (parseInt(process.argv[i]) >= value1) {
       value1 = parseInt(process.argv[i]);
-    }
+    } 
+  }
+  if (value1 === value2) {
+    value2 = parseInt(process.argv[3]);
+  }
+  for (let i = 2; i < process.argv.length; i++) {
     if (parseInt(process.argv[i]) >= value2 && parseInt(process.argv[i]) < value1) {
       value2 = parseInt(process.argv[i]);
     }
