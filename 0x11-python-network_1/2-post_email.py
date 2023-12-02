49aa61318e7416d1d3455e5c1fe727763d560675
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-12-01 18:22:03 -0800

    POST an email #0

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module for task "POST an email #0"
+"""
+import urllib.parse
+import urllib.request
+import sys
+values = {'email': sys.argv[2]}
+data = urllib.parse.urlencode(values)
+data = data.encode('ascii')
+req = urllib.request.Request(sys.argv[1], data)
+with urllib.request.urlopen(req) as response:
+    the_page = response.read()
+    print(the_page.decode('utf-8'))
