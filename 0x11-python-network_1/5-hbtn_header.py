f6741004151c026f1405e1f120880cc35eb3ff8f
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-12-01 18:55:21 -0800

    Response header value #1

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+This module for task "Response header value #1"
+"""
+if __name__ == "__main__":
+    import requests
+    import sys
+    r = requests.get(sys.argv[1])
+    print(r.headers['X-Request-Id'])
