ea0cee9ec9a6a9f9cb5cb5e4f85f630cfed946b9
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2024-01-03 23:46:54 -0800

    Status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv.slice(2)[0];
+request(url, function (error, response, body) {
+	console.log("code: " + response.statusCode);
+});
