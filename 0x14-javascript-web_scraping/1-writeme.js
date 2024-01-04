fffe55a8324489e7e8decab6127e26b338bfb088
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2024-01-03 23:31:58 -0800

    Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const file = process.argv.slice(2)[0];
 const content = process.argv.slice(2)[1];
 fs.writeFile(file, content, (error) => {
-    if (error) {
-	    throw error
-    }
+  if (error) {
+    throw error;
+  }
 });
