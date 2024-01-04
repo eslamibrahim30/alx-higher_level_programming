bcf21c18b508fa9078d3dee021443460200750df
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2024-01-03 23:49:15 -0800

    Status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,5 +2,8 @@
 const request = require('request');
 const url = process.argv.slice(2)[0];
 request(url, function (error, response, body) {
-	console.log("code: " + response.statusCode);
+  if (error) {
+    throw error;
+  }
+  console.log('code: ' + response.statusCode);
 });
