91509290829ca1f144f52d29109666458965d007
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2024-01-03 23:24:10 -0800

    Write me

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const fs = require('fs');
+const file = process.argv.slice(2)[0];
+const content = process.argv.slice(2)[1];
+fs.writeFile(file, content, (error) => {
+    if (error) {
+	    throw error
+    }
+});
