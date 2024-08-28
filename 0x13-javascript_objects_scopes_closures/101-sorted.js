96146e38fbddaab1544230be59e8cd8fc05d3609
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2024-08-28 06:51:39 +0300

    Sorted occurences

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -2,7 +2,7 @@
 const { dict } = require('./101-data.js');
 const newDict = {};
 for (const [key, value] of Object.entries(dict)) {
-  if (!(Object.prototype.hasOwnProperty.call(newDict, value))) {
+  if (!(value in newDict)) {
     newDict[value] = [];
   }
   newDict[value].push(key);
