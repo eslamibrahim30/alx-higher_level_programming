1410384f232811f70b6cc14cd3f42fdf7653086f
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2024-09-06 18:17:30 +0300

    Linked list palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,6 +1,73 @@
 #include "lists.h"
 #include <stdlib.h>
 
+
+/**
+ * list_len - Calculates the length of a given non-looping singly linked list.
+ * @head: a pointer to the head of the linked list.
+ *
+ * Return: the length of a given singly linked list.
+ */
+int list_len(listint_t *head)
+{
+	int len = 0;
+	listint_t *ptr = head;
+
+	while (ptr != NULL)
+	{
+		ptr = ptr->next;
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * get_middle - Returns a pointer to the middle node of a given
+ * non-looping singly linked list.
+ * @head: a pointer to the head of the linked list.
+ * @len: the length of the linked list.
+ *
+ * Return: a pointer to the middle node of the list.
+ */
+listint_t *get_middle(listint_t *head, int len)
+{
+	int mid_idx = 0;
+	int i = 0;
+	listint_t *ptr = head;
+
+	mid_idx = (len / 2) + (len % 2) - 1;
+	for (i = 0; i <= mid_idx; i++)
+		ptr = ptr->next;
+	return (ptr);
+}
+
+/**
+ * reverse_list - Reverses a singly linked list.
+ * @head: a pointer to the head of the linked list.
+ *
+ * Return: a pointer to the head of the list.
+ */
+listint_t *reverse_list(listint_t *head)
+{
+	listint_t *ptr = NULL;
+	listint_t *ptr_next = NULL;
+	listint_t *temp = NULL;
+
+	if (head == NULL)
+		return (NULL);
+	ptr = head;
+	ptr_next = ptr->next;
+	while (ptr_next != NULL)
+	{
+		temp = ptr_next->next;
+		ptr_next->next = ptr;
+		ptr = ptr_next;
+		ptr_next = temp;
+	}
+	head->next = NULL;
+	return (ptr);
+}
+
 /**
  * is_palindrome - Checks if a singly linked list is a palindrome.
  * @head: a pointer to the head of the linked list.
@@ -9,42 +76,33 @@
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *ptr = NULL;
-	int *list_arr = NULL;
 	int len = 0;
-	int half_len = 0;
-	int i = 0;
+	int is_palindrome_list = 1;
+	listint_t *ptr_1 = NULL;
+	listint_t *ptr_2 = NULL;
+	listint_t *middle = NULL;
+	listint_t *reversed_second_half = NULL;
 
 	if (head == NULL)
 		return (0);
 	if (*head == NULL)
 		return (1);
-	ptr = *head;
-	while (ptr != NULL)
+	len = list_len(*head);
+	middle = get_middle(*head, len);
+	reversed_second_half = reverse_list(middle->next);
+	middle->next = NULL;
+	ptr_1 = *head;
+	ptr_2 = reversed_second_half;
+	while (ptr_1 != NULL && ptr_2 != NULL)
 	{
-		ptr = ptr->next;
-		len++;
-	}
-	list_arr = (int *)malloc(len * sizeof(int));
-	if (list_arr == NULL)
-		return (0);
-	ptr = *head;
-	i = 0;
-	while (ptr != NULL)
-	{
-		list_arr[i] = ptr->n;
-		ptr = ptr->next;
-		i++;
-	}
-	half_len = len / 2;
-	for (i = 0; i <= half_len; i++)
-	{
-		if (list_arr[i] != list_arr[len - i - 1])
+		if (ptr_1->n != ptr_2->n)
 		{
-			free(list_arr);
-			return (0);
+			is_palindrome_list = 0;
+			break;
 		}
+		ptr_1 = ptr_1->next;
+		ptr_2 = ptr_2->next;
 	}
-	free(list_arr);
-	return (1);
+	middle->next = reverse_list(reversed_second_half);
+	return (is_palindrome_list);
 }
