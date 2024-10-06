7783aa67794f5ff175104c2d37d27bd860b3f472
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2024-10-06 10:48:59 +0300

    Integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,8 +10,11 @@ def add_integer(a, b=98):
     """This function adds two numbers a and b.
     a and b must be integers or floats, otherwise raise a TypeError
     a and b must be first casted to integers if they are float."""
-    if type(a) not in [int, float]:
+    if type(a) not in [int, float] or a != a:
         raise TypeError("a must be an integer")
-    if type(b) not in [int, float]:
+    if type(b) not in [int, float] or b != b:
         raise TypeError("b must be an integer")
+    result = a + b
+    if result == float('inf') or result == -float('inf'):
+        return -1
     return int(a) + int(b)
