feb461728e74eb43f25db0e5a25599e2d72c03d3
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2024-10-07 06:59:55 +0300

    CPython #3: Python Strings

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -6,7 +6,6 @@ void print_python_string(PyObject *p)
 {
 	const char *string = NULL;
 	Py_ssize_t size = 0;
-	int type = 0;
 
 	printf("[.] string object info\n");
 	if (PyUnicode_Check(p) == 0)
@@ -14,10 +13,9 @@ void print_python_string(PyObject *p)
 		printf("  [ERROR] Invalid String Object\n");
 		return;
 	}
-	type = PyUnicode_KIND(p);
 	size = PyUnicode_GET_LENGTH(p);
 	string = PyUnicode_AsUTF8(p);
-	if (type == 1)
+	if (PyUnicode_IS_COMPACT_ASCII(p) == 1)
 		printf("  type: compact ascii\n");
 	else
 		printf("  type: compact unicode object\n");
