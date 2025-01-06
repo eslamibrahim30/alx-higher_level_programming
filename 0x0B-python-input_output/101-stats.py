34f67108310a6f432541ac4dfd3bbdfb85c2037d
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2025-01-06 18:48:43 +0200

    Log parsing

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -4,8 +4,10 @@ This module for log parsing.
 """
 if __name__ == "__main__":
     import sys
+    import re
     line_c = 0
     line_list = None
+    p = re.compile('\d+\.\d+\.\d+\.\d+ \- \[\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+\] "[a-zA-Z]+ \S+ \S+" \d+ \d+', re.IGNORECASE)
     total_size = 0
     msg = ""
     s_codes = {
@@ -20,24 +22,32 @@ if __name__ == "__main__":
     }
     try:
         for line in sys.stdin:
+            if not p.match(line):
+                continue
             line_list = line.split()
+            total_size += int(line_list[-1])
+            s_codes[line_list[-2]] += 1
+            line_c += 1
             if line_c == 10:
                 msg = ""
                 msg += "File size: {}\n".format(total_size)
                 for i in s_codes:
-                    if s_codes[i] > 0:
+                    if s_codes[i] != 0:
                         msg += "{}: {}\n".format(i, s_codes[i])
-                print(msg, end="")
+                sys.stdout.write(msg)
                 line_c = 0
-            else:
-                total_size += int(line_list[-1])
-                s_codes[line_list[-2]] += 1
-                line_c += 1
+        if line_c > 0 or total_size == 0:
+            msg = ""
+            msg += "File size: {}\n".format(total_size)
+            for i in s_codes:
+                 if s_codes[i] != 0:
+                     msg += "{}: {}\n".format(i, s_codes[i])
+            sys.stdout.write(msg)
     except KeyboardInterrupt:
         msg = ""
         msg += "File size: {}\n".format(total_size)
         for i in s_codes:
             if s_codes[i] > 0:
                 msg += "{}: {}\n".format(i, s_codes[i])
-        print(msg, end="")
+        sys.stdout.write(msg)
         raise
