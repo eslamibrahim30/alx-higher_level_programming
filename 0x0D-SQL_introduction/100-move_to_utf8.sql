0f317b9e1cc047d26b4717d9ebc1e2bdc5f3cbf4
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2025-01-08 16:05:16 +0200

    Go to UTF8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,5 @@
 -- This script converts hbtn_0c_0 database to UTF8
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4; 
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4;
-ALTER TABLE first_table MODIFY name CHARACTER SET utf8mb4;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci; 
+USE hbtn_0c_0;
+ALTER TABLE first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table MODIFY COLUMN name VARCHAR(256) COLLATE utf8mb4_unicode_ci DEFAULT NULL;
